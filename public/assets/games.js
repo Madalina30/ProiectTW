6171fe08d150ca2a919e59fc903afaaf891fd023
Madalina30 <mada.banica@yahoo.com>
2021-05-07 13:51:01 +0300

    Go to the good level

--- public/assets/games.js
+++ public/assets/games.js
@@ -50,28 +50,27 @@ async function buildCategry(catZone, catData){
     }
     catZone.innerHTML = catHolder;
     document.querySelector(".beginner-html").addEventListener('click', function() {
-        window.location.href = "templateCategory.php?cat=hb&level=0"
-        // sa se duca la nivelul la care a ramas - db
+        window.location.href = "templateCategory.php?cat=hb&level=" + document.querySelector('.config .htmlBeginner .level').innerText;
     });
     
     document.querySelector(".intermediate-html").addEventListener('click', function() {
-        window.location.href = "templateCategory.php?cat=hi&level=0"
+        window.location.href = "templateCategory.php?cat=hi&level=" + document.querySelector('.config .htmlIntermediate .level').innerText;
     });
     
     document.querySelector(".expert-html").addEventListener('click', function() {
-        window.location.href = "templateCategory.php?cat=he&level=0"
+        window.location.href = "templateCategory.php?cat=he&level=" + document.querySelector('.config .htmlExpert .level').innerText;
     });
     
     document.querySelector(".beginner-css").addEventListener('click', function() {
-        window.location.href = "templateCategory.php?cat=cb&level=0"
+        window.location.href = "templateCategory.php?cat=cb&level=" + document.querySelector('.config .cssBeginner .level').innerText;
     });
     
     document.querySelector(".intermediate-css").addEventListener('click', function() {
-        window.location.href = "templateCategory.php?cat=ci&level=0"
+        window.location.href = "templateCategory.php?cat=ci&level=" + document.querySelector('.config .cssIntermediate .level').innerText;
     });
     
     document.querySelector(".expert-css").addEventListener('click', function() {
-        window.location.href = "templateCategory.php?cat=ce&level=0"
+        window.location.href = "templateCategory.php?cat=ce&level=" + document.querySelector('.config .cssExpert .level').innerText;
     });
 }
 
