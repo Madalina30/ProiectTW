d033a7f9d393aaa9c4187a1e3abf18deb4dde7af
Madalina30 <mada.banica@yahoo.com>
2021-05-20 18:35:00 +0300

    small change

--- public/assets/showMore.js
+++ public/assets/showMore.js
@@ -1,15 +1,16 @@
 const buttonShowMore = document.querySelector('.btn__show');
-const section = document.querySelector("#about");
+const section = document.querySelector('#about');
 let paragraph = document.createElement("p", HTMLParagraphElement);
 let link = document.createElement("a", HTMLLinkElement);
 let buttonShowLess = document.createElement("button", HTMLButtonElement);
 
 buttonShowMore.addEventListener('click', () => {
     buttonShowMore.style.display = "none";
-    paragraph.innerHTML = "Here is the link to the amazing guide that explains you the game: <a href='https://lego-hmlcss.000webhostapp.com/app/views/documentatie.html' style='color:blueviolet'>Guide</a>"
+    paragraph.innerHTML = "Here is the link to the amazing guide that explains you the game: <a href='https://lego-hmlcss.000webhostapp.com/app/views/documentatie.html' style='color:blueviolet'>Guide</a>";
     buttonShowLess.className = "btn__show";
     buttonShowLess.type = "button";
     buttonShowLess.innerText = "Show less";
+    buttonShowLess.style.cursor = 'pointer';
 
     section.appendChild(paragraph);
     section.appendChild(buttonShowLess);
